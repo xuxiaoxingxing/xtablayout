d8406590e7d22da97cf469e67e99da1d17bb092b
小星 <753940262@qq.com>
2017-09-18 16:28:56 +0800

    1、底部导航栏

---
+++ xtablayout/src/main/java/com/xiaoxing/adapter/ViewPagerAdapter.java
@@ -0,0 +1,42 @@
+package com.xiaoxing.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 描述：
+ * 作者：xiaoxing on 17/5/9 10:34
+ * 邮箱：2235445233@qq.com
+ */
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFrag(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+}
